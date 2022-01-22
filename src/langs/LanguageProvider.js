673ff1a1eaf4cec7ec65c6cf4533400373bd8043
dylan.tsou <dylan.tsou@vivicloud.net>
2022-01-22 14:39:13 +0800

    fix comment

--- src/langs/LanguageProvider.js
+++ src/langs/LanguageProvider.js
@@ -13,6 +13,11 @@ export const LanguageProvider = ({ children }) => {
 
   const fetchData = async () => {
     let lang = await localforage.getItem("rcml-lang");
+    /** 
+     * To prevent error about someday someone to change lang name e.g. 'zh_tw' => 'tw', and user still have old rcml-lang value 'zh_tw' in browser indexDB, 
+     * so give it below conditional expressions to see if now includes this value, if not give it to default value 'en' 
+     * By Dylan 
+     * */
     if (!Object.keys(dictionaryList).includes(lang)) {
       await localforage.setItem("rcml-lang", "us");
       setUserLanguage("us");
