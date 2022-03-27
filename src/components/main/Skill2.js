5e42ef882d950d1d92695f011ff4384904908515
Dylan <bear817005@gmail.com>
2022-03-27 09:30:49 +0800

    add php laravel skill

--- src/components/main/Skill2.js
+++ src/components/main/Skill2.js
@@ -1,40 +1,12 @@
 import React from "react";
 import { useTranslation } from "../../langs/useTranslation";
 import Tooltip from "@mui/material/Tooltip";
-import vueLogo from "../../assets/images/vue.png";
-import angularLogo from "../../assets/images/angular.png";
-import herokuLogo from "../../assets/images/heroku.png";
-import gitLogo from "../../assets/images/git.png";
-import sassLogo from "../../assets/images/sass.png";
-import reactLogo from "../../assets/images/react.png";
-import nodeLogo from "../../assets/images/node.png";
-import htmlSetLogo from "../../assets/images/htmlset.png";
-import mysqlLogo from "../../assets/images/mysql.png";
-import typescriptLogo from "../../assets/images/typescript.png";
-import mongoDBLogo from "../../assets/images/mongodb.png";
-import sketchLogo from "../../assets/images/sketch.svg";
-import jqueryLogo from "../../assets/images/jquery.png";
-import materialLogo from "../../assets/images/material.svg";
+import { toolsSet1 } from "../../helpers/skillsImg";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
-const Skill = (props) => {
+const Skill = () => {
   const { t } = useTranslation("skill");
-  const toolsSet1 = [
-    { src: reactLogo, name: "React" },
-    { src: angularLogo, name: "Angular" },
-    { src: vueLogo, name: "Vue / Nuxt" },
-    { src: nodeLogo, name: "Node.js / Express" },
-    { src: mongoDBLogo, name: "MongoDB" },
-    { src: htmlSetLogo, name: "Html + CSS + Javascript" },
-    { src: typescriptLogo, name: "Typescript" },
-    { src: jqueryLogo, name: "jQuery" },
-    { src: sassLogo, name: "Sass / Less / Scss" },
-    { src: gitLogo, name: "Git / Github / Gitlab / Bitbucket" },
-    { src: mysqlLogo, name: "SQL Server" },
-    { src: herokuLogo, name: "Heroku" },
-    { src: sketchLogo, name: "Sketch" },
-    { src: materialLogo, name: "Material UI / Ant Design / Bootstrap..." },
-  ];
+  
   return (
     <div className="skill section">
       <div className="title">
---
+++ src/helpers/skillsImg.js
@@ -0,0 +1,35 @@
+import vueLogo from "../assets/images/vue.png";
+import angularLogo from "../assets/images/angular.png";
+import herokuLogo from "../assets/images/heroku.png";
+import gitLogo from "../assets/images/git.png";
+import sassLogo from "../assets/images/sass.png";
+import reactLogo from "../assets/images/react.png";
+import nodeLogo from "../assets/images/node.png";
+import htmlSetLogo from "../assets/images/htmlset.png";
+import mysqlLogo from "../assets/images/mysql.png";
+import typescriptLogo from "../assets/images/typescript.png";
+import mongoDBLogo from "../assets/images/mongodb.png";
+import sketchLogo from "../assets/images/sketch.svg";
+import jqueryLogo from "../assets/images/jquery.png";
+import laravelLogo from "../assets/images/laravel.png";
+import phpLogo from "../assets/images/php.png";
+import materialLogo from "../assets/images/material.svg";
+
+export const toolsSet1 = [
+  { src: reactLogo, name: "React" },
+  { src: angularLogo, name: "Angular" },
+  { src: vueLogo, name: "Vue / Nuxt" },
+  { src: nodeLogo, name: "Node.js / Express" },
+  { src: mongoDBLogo, name: "MongoDB" },
+  { src: htmlSetLogo, name: "Html + CSS + Javascript" },
+  { src: typescriptLogo, name: "Typescript" },
+  { src: jqueryLogo, name: "jQuery" },
+  { src: sassLogo, name: "Sass / Less / Scss" },
+  { src: gitLogo, name: "Git / Github / Gitlab / Bitbucket" },
+  { src: phpLogo, name: "PHP" },
+  { src: laravelLogo, name: "Laravel" },
+  { src: mysqlLogo, name: "SQL Server" },
+  { src: herokuLogo, name: "Heroku" },
+  { src: sketchLogo, name: "Sketch" },
+  { src: materialLogo, name: "Material UI / Ant Design / Bootstrap..." },
+];
