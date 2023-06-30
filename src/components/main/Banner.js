15c4dbafb90afd5aff582dc8974a4cb9e5c9553d
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2023-06-30 23:21:44 +0800

    fix photo

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "langs/useTranslation";
-import { ReactComponent as SelfPhoto } from "assets/images/self.svg";
+import SelfPhoto from "assets/images/self.png";
 import mainBall from "assets/images/mainBall.svg";
 import bannerShape from "assets/images/banner-shape.png";
 import ReactTypingEffect from "react-typing-effect";
@@ -39,7 +39,7 @@ const Banner = () => {
         </div>
         <div className="banner-right">
           <div className="banner-img">
-            <SelfPhoto className="selfPhoto" />
+            <img src={SelfPhoto} alt="selfPhoto" className="selfPhoto" />
             <img src={mainBall} alt="mainBall" className="mainBall" />
           </div>
         </div>
